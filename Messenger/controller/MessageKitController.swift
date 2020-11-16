2dddb6796999d3fd7ad0626b0b99d83b104433a8
ian rust <ibrust777@gmail.com>
2020-11-16 13:07:03 -0500

    pushing to GitHub

--- Messenger/controller/MessageKitController.swift
+++ Messenger/controller/MessageKitController.swift
@@ -12,11 +12,16 @@ import MessageKit
 /*
 class Message_Kit_Controller : MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
     
+    // I don't know how you'd change the sender... there's no indexPath ... maybe it's always me?
     func currentSender() -> SenderType {
         
     }
     
+    // I think this is the equivalent of cellforrowat
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        
+        // I believe the MessageType is already defined... same with Sender ... 
+ 
     }
     
     func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
